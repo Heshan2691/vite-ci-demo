1f3ebece0b115bd5df09d3df8e808c202fc46f19
Heshan Yatigammana <131267985+Heshan2691@users.noreply.github.com>
2026-02-24 22:55:30 +0530

    Generate coverage reports

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,10 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
   },
   assetsInclude: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif"],
 });
